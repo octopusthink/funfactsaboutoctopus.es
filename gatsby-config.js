40b63278c2d22d94b5bce374f7d7b1120839b9ee
sarah <sarah@triggersandsparks.com>
2019-10-07 21:00:29 +0100

    feat: Add curly quotes.

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,11 @@ module.exports = {
         ignore: [`**/*\.js`],
       },
     },
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: ['gatsby-remark-smartypants'],
+      },
+    },
   ],
 };
