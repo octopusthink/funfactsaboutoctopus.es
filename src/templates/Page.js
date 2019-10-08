b0f60211feefa5c11a9464318a9985f7826bb474
sarah <sarah@triggersandsparks.com>
2019-10-08 01:43:56 +0100

    feat: Ensure site works properly on a mobile device.

--- src/templates/Page.js
+++ src/templates/Page.js
@@ -26,13 +26,14 @@ export const Page = (props) => {
       <section
         css={css`
           display: grid;
-          grid-template-columns: 1fr 48rem 4.8rem;
-          //border: 10px solid black;
           grid-gap: 4.8rem;
-          //padding: 4.8rem;
           align-items: center;
           justify-content: center;
           min-height: 100vh;
+
+          @media screen and (min-width: 640px) {
+            grid-template-columns: 1fr 48rem 4.8rem;
+          }
         `}
       >
         <div
@@ -52,12 +53,28 @@ export const Page = (props) => {
               display: block;
               object-fit: cover;
               filter: grayscale(90%) brightness(110%) contrast(90%);
+
+              @media screen and (max-width: 639px) {
+                position: fixed;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+              }
             `}
           />
         </div>
         <section
           css={css`
-            //margin: 3.2rem auto;
+            @media screen and (max-width: 639px) {
+              background: rgba(255, 255, 255, 0.95);
+              margin: 0 2.4rem;
+              padding: 2.4rem;
+              position: absolute;
+              top: 32rem;
+              box-shadow: 0 4px 10px rgba(0, 0, 0, 0.125);
+            }
+
             letter-spacing: -0.0125em;
           `}
         >
