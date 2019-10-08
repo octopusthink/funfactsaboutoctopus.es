0986797008ba6be581cc875f1989d4a0feda7470
sarah <sarah@triggersandsparks.com>
2019-10-08 15:02:29 +0100

    fix: Use separate templates for pages and facts.
    This implements a PageWrapper component for the reusable styles, but 
    uses slightly different styling depending on the type of content being 
    shown. Also fixes the issue where pages were showing an error.

---
+++ src/components/PageWrapper/index.js
@@ -0,0 +1,68 @@
+import { Icon, Link } from '@octopusthink/nautilus';
+import React from 'react';
+import { css } from '@emotion/core';
+
+const PageWrapper = (props) => {
+  const { image, children } = props;
+  return (
+    <main
+      css={css`
+        display: grid;
+        grid-gap: 4.8rem;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+
+        @media screen and (min-width: 640px) {
+          grid-template-columns: 1fr 48rem 4.8rem;
+        }
+      `}
+    >
+      <div
+        css={css`
+          height: 100vh;
+          overflow: hidden;
+        `}
+      >
+        <img
+          alt=""
+          src={image}
+          css={css`
+            width: 100%;
+            height: 100%;
+            max-height: 100%;
+            max-width: 100%;
+            display: block;
+            object-fit: cover;
+            filter: grayscale(90%) brightness(110%) contrast(90%);
+
+            @media screen and (max-width: 639px) {
+              position: fixed;
+              top: 0;
+              left: 0;
+              right: 0;
+              bottom: 0;
+            }
+          `}
+        />
+      </div>
+      <section
+        css={css`
+          @media screen and (max-width: 639px) {
+            background: rgba(255, 255, 255, 0.95);
+            margin: 0 2.4rem;
+            padding: 2.4rem;
+            position: absolute;
+            top: 32rem;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.125);
+          }
+          letter-spacing: -0.0125em;
+        `}
+      >
+        {children}
+      </section>
+    </main>
+  );
+};
+
+export default PageWrapper;
--- src/templates/Fact.js
+++ src/templates/Fact.js
@@ -1,4 +1,89 @@
-import Page from './Page';
+import { css } from '@emotion/core';
+import { Heading, Link, Paragraph } from '@octopusthink/nautilus';
+import { graphql } from 'gatsby';
+import React from 'react';
+import Helmet from 'react-helmet';
+
+import { markdown } from '../utils/markdown';
+
+import App from './App';
+import PageWrapper from '../components/PageWrapper';
+
+export const Page = (props) => {
+  const { data, pageContext } = props;
+  const { allFactSlugs, nextSlug, previousSlug } = pageContext;
+
+  const { page } = data;
+  const { htmlAst } = page;
+  const { image, number, title, source, sourceUrl } = page.fields;
+
+  const content = markdown(htmlAst);
+
+  const randomSlug = () => {
+    return allFactSlugs[Math.floor(Math.random() * allFactSlugs.length)];
+  };
+  const twoDigitNumber = number < 10 ? '0' + number : number;
+
+  return (
+    <App>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+
+      <PageWrapper image={image}>
+        <Heading
+          css={css`
+            position: relative;
+          `}
+        >
+          {number && (
+            <span
+              css={css`
+                position: absolute;
+                top: -12rem;
+                left: -8rem;
+                font-size: 23rem;
+                letter-spacing: -0.07em;
+                opacity: 0.15;
+                color: #fefefe;
+                text-shadow:-4px -4px 0 #666,
+                -4px 4px 0 #666,4px -4px 0 #666,
+                4px 4px 0 #666; }
+
+                @media screen and (min-width: 640px) {
+                  top: -10rem;
+                  left: -18rem;
+                }
+              `}
+            >
+              {twoDigitNumber}
+            </span>
+          )}
+          <span
+            css={css`
+              position: relative;
+            `}
+          >
+            {title}
+          </span>
+        </Heading>
+        {content}
+
+        {sourceUrl && (
+          <Paragraph
+            css={css`
+              font-style: italic;
+            `}
+          >
+            <Link href={sourceUrl} as="a" external>
+              Source: {source}
+            </Link>
+          </Paragraph>
+        )}
+      </PageWrapper>
+    </App>
+  );
+};
 
 export const pageQuery = graphql`
   query($id: String!) {
